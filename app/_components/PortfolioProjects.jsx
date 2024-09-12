1a4adb626a70b1a1f90e05f426d6907f933a35bd
Ana Škarica <ana.skarica.as@gmail.com>
2024-09-12 09:08:12 +0200

    Fix bug not being able to check portfolio page

--- app/_components/PortfolioProjects.jsx
+++ app/_components/PortfolioProjects.jsx
@@ -7,7 +7,6 @@ import {
 } from "../_libs/utils";
 import ProjectCard from "./ProjectCard";
 import { useRouter } from "next/navigation";
-import { deepEqual } from "@/utils/common/methods";
 
 const PortfolioProjects = () => {
   const router = useRouter();
@@ -27,34 +26,6 @@ const PortfolioProjects = () => {
     }
   }, [portfolioStackHeroContextData]);
 
-  const checkIfPortfolioStackProjectsContextDataIsDefault = () => {
-    const defaultValue = [
-      {
-        id: null,
-        project_order: 1,
-        project_title: "",
-        project_description: "",
-        project_technology_1_icon: "",
-        project_technology_2_icon: "",
-        project_technology_3_icon: "",
-        project_technology_4_icon: "",
-        project_technology_5_icon: "",
-        project_link_1_url: "",
-        project_link_2_url: "",
-        project_link_1_text: "",
-        project_link_2_text: "",
-        project_link_1_icon: "",
-        project_link_2_icon: "",
-        project_img: "",
-      },
-    ];
-
-    return (
-      deepEqual(portfolioStackProjectsContextData, defaultValue) ||
-      deepEqual(portfolioStackProjectsContextData, [])
-    );
-  };
-
   return (
     <div
       className={`w-full 
@@ -63,43 +34,35 @@ const PortfolioProjects = () => {
       <div
         className={`max-w-screen-lg px-4 pb-2 md:px-2 mx-auto pt-16 flex flex-col gap-8`}
       >
-        {portfolioStackHeroContextData &&
-          portfolioStackHeroContextData.project_group_title && (
-            <h3
-              className={`uppercase text-xl font-medium tracking-wider font-${fontPrimary} ${getPrimaryColors(
-                palette
-              )}`}
-            >
-              {portfolioStackHeroContextData.project_group_title}
-            </h3>
-          )}
-        {portfolioStackHeroContextData &&
-          portfolioStackHeroContextData.project_group_description && (
-            <p className={`text-xl font-bold max-w-xs font-${fontPrimary}`}>
-              {portfolioStackHeroContextData.project_group_description}
-            </p>
-          )}
-        {!checkIfPortfolioStackProjectsContextDataIsDefault() && (
-          <div
-            className={`flex flex-col w-full gap-10 md:gap-16 ${
-              projectsDesign == "Design 2" ||
-              projectsDesign == "Design 3" ||
-              projectsDesign == "Design 4"
-                ? "grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-8"
-                : ""
-            }`}
-          >
-            {portfolioStackProjectsContextData.map((el, index) => (
-              <ProjectCard
-                project={el}
-                key={index}
-                projectIndex={index}
-                projectsDesign={projectsDesign}
-                projectsAlignment={projectsAlignment}
-              />
-            ))}
-          </div>
-        )}
+        <h3
+          className={`uppercase text-xl font-medium tracking-wider font-${fontPrimary} ${getPrimaryColors(
+            palette
+          )}`}
+        >
+          {portfolioStackHeroContextData.project_group_title}
+        </h3>
+        <p className={`text-xl font-bold max-w-xs font-${fontPrimary}`}>
+          {portfolioStackHeroContextData.project_group_description}
+        </p>
+        <div
+          className={`flex flex-col w-full gap-10 md:gap-16 ${
+            projectsDesign == "Design 2" ||
+            projectsDesign == "Design 3" ||
+            projectsDesign == "Design 4"
+              ? "grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-8"
+              : ""
+          }`}
+        >
+          {portfolioStackProjectsContextData.map((el, index) => (
+            <ProjectCard
+              project={el}
+              key={index}
+              projectIndex={index}
+              projectsDesign={projectsDesign}
+              projectsAlignment={projectsAlignment}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- app/_components/RenderedPortfolio.jsx
+++ app/_components/RenderedPortfolio.jsx
@@ -89,6 +89,52 @@ const RenderedPortfolio = ({ domain = null }) => {
     );
   };
 
+  const checkIfPortfolioStackProjectsContextDataIsDefault = () => {
+    const defaultValue = [
+      {
+        id: null,
+        project_order: 1,
+        project_title: "",
+        project_description: "",
+        project_technology_1_icon: "",
+        project_technology_2_icon: "",
+        project_technology_3_icon: "",
+        project_technology_4_icon: "",
+        project_technology_5_icon: "",
+        project_link_1_url: "",
+        project_link_2_url: "",
+        project_link_1_text: "",
+        project_link_2_text: "",
+        project_link_1_icon: "",
+        project_link_2_icon: "",
+        project_img: "",
+      },
+    ];
+
+    return (
+      deepEqual(portfolioStackProjectsContextData, defaultValue) ||
+      deepEqual(portfolioStackProjectsContextData, [])
+    );
+  };
+
+  const checkIfPortfolioStackExperienceContextDataIsDefault = () => {
+    const defaultValue = [
+      {
+        id: null,
+        experience_order: 1,
+        experience_title: "",
+        experience_description: "",
+        experience_time: "",
+        experience_keywords: "",
+        experience_type: null,
+      },
+    ];
+    return (
+      deepEqual(portfolioStackExperienceContextData, defaultValue) ||
+      deepEqual(portfolioStackExperienceContextData, [])
+    );
+  };
+
   const checkIfPortfolioStackActivityContextDataIsDefault = () => {
     const defaultValue = [
       {
@@ -130,8 +176,14 @@ const RenderedPortfolio = ({ domain = null }) => {
                 <PortfolioHero /> {domain && <MadeUsingTag />}
               </div>
             )}
-          {portfolioStackProjectsContextData && <PortfolioProjects />}
-          {portfolioStackExperienceContextData && <PortfolioEducation />}
+          {portfolioStackProjectsContextData &&
+            !checkIfPortfolioStackProjectsContextDataIsDefault() && (
+              <PortfolioProjects />
+            )}
+          {portfolioStackExperienceContextData &&
+            !checkIfPortfolioStackExperienceContextDataIsDefault() && (
+              <PortfolioEducation />
+            )}
           {portfolioStackActivityContextData &&
             !checkIfPortfolioStackActivityContextDataIsDefault() && (
               <PortfolioActivities />
