2924cf181329a076582ffa4a1b103267fd677e9f
Ana Škarica <ana.skarica.as@gmail.com>
2024-09-20 19:35:13 +0200

    Add error page UI

--- app/(web-builder)/error/page.jsx
+++ app/(web-builder)/error/page.jsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import errorImage from "@/public/error.png";
+
 const ErrorPage = () => {
-  return <p>Sorry, something went wrong</p>;
+  return (
+    <div className="flex flex-col gap-8 justify-center items-center h-full mt-12">
+      <Image src={errorImage} alt="error icon" width={256} height={256} />
+      <p className="text-xl"> Sorry, something went wrong. Please try again.</p>
+      <p>
+        If it still doesn&apos;t work, please{" "}
+        <Link href={"/send-feedback"} className="underline">
+          contact us
+        </Link>
+        .
+      </p>
+    </div>
+  );
 };
 
 export default ErrorPage;
